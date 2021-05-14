4be8dfc2b9696c176b6076e3b78a181860d84d72
tzyn (mac) <tzyn.wang@gmail.com>
2021-05-14 23:37:46 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express()
 const port = 3000
 
 const expressHandlebars = require('express-handlebars')
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+const handlebarsHelpers = require('handlebars-helpers')
+const helpers = handlebarsHelpers()
+const bodyParser = require('body-parser')
+
+app.engine('handlebars', expressHandlebars({ helpers: helpers, defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 const restaurantList = require('./restaurant.json')
@@ -36,6 +40,31 @@ app.get('/search', (req, res) => {
     : matchResult = false
   const keywordSpan = `<span>${keyword}</span>`
   res.render('index', { keyword: keywordSpan, matchResult: matchResult, restaurants: searchResult })
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id)
+  const targetRestaurant = restaurantList.results.find((restaurant) => restaurant.id === id)
+  const categories = []
+  restaurantList.results.forEach(restaurant => {
+    if (!categories.includes(restaurant.category)) {
+      categories.push(restaurant.category)
+    }
+  })
+  res.render('edit', { restaurant: targetRestaurant, categories })
+})
+
+app.put('/restaurants/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+  const targetRestaurant = restaurantList.results.find((restaurant) => restaurant.id === id)
+  // 遍歷req.body物件中所有的內容
+  for (const key in req.body) {
+    if (req.body[key].length > 0) {
+      targetRestaurant[key] = req.body[key]
+    }
+  }
+  res.redirect('/')
 })
 
 app.delete('/:id', (req, res) => {
