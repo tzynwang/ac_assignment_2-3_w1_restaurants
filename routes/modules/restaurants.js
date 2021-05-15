8898c92869a810d72e100270eacf52cf914a1639
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-05-15 20:42:25 +0800

    feat: add 'add restaurant' function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,6 +2,39 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
+// add restaurant (view)
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(allRestaurants => {
+      const categories = []
+      allRestaurants.forEach(restaurant => {
+        if (!categories.includes(restaurant.category)) {
+          categories.push(restaurant.category)
+        }
+      })
+      res.render('add', { categories })
+    })
+    .catch(error => console.log(error))
+})
+
+// add restaurant (save)
+router.post('/', (req, res) => {
+  Restaurant.create({
+    name: req.body.name,
+    name_en: req.body.name_en,
+    category: req.body.category,
+    image: req.body.image,
+    location: req.body.location,
+    phone: req.body.phone,
+    google_map: `http://maps.google.com/maps?z=12&t=m&q=${req.body.name}`,
+    rating: req.body.rating,
+    description: req.body.description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // detail page
 router.get('/:id', (req, res) => {
   const id = req.params.id
